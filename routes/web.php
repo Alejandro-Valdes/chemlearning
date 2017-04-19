5a6e72d41a37a415a049c047cb33a0357c40e83e
manolo <manolosavi@me.com>
2017-04-19 11:22:27 -0500

    Changed CACHE_DRIVER to array to fix the error: “This cache store does not support tagging”. Added /setup-entrust page to create admin role and permissions.

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,32 @@ Route::get('/topic/{topic_id}/new-question', 'QuestionController@create');
 Route::post('/topic/{topic_id}/new-question', 'QuestionController@store');
 Route::get('/topic/{topic_id}/question/{id}', 'QuestionController@show');
 
+use App\Role as Role;
+use App\Permission as Permission;
+Route::get('setup-entrust', function() {
+	// para que solo admin pueda agregar / modificar / borrar
+	$admin = new Role();
+	$admin->name = 'Admin';
+	$admin->save();
 
+	$add = new Permission();
+	$add->name = 'can_add';
+	$add->save();
 
+	$modify = new Permission();
+	$modify->name = 'can_modify';
+	$modify->save();
 
+	$delete = new Permission();
+	$delete->name = 'can_add';
+	$delete->save();
+
+	$admin->attachPermission($add);
+	$admin->attachPermission($modify);
+	$admin->attachPermission($delete);
+
+	// $user1 = User::find(1);
+	// $user1->attachRole($admin);
+
+	return 'success';
+});
